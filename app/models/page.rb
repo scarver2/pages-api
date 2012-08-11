39473f126dd4a85747e7306ce9d2b3c16502d753
Stan Carver <stan.carver@gatewaypeople.com>
2012-08-11 16:28:58 -0500

    added page validations.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,10 @@
 class Page < ActiveRecord::Base
   attr_accessible :content, :published_on, :title
+  
+  validates_presence_of :content, :title
+  validates_uniqueness_of :title
+
+  def total_word_count
+    content.split.size + title.split.size
+  end
 end
