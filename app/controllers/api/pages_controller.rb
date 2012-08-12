6e76c11221e191c14d1dcb2d4b43d422d4455ee1
Stan Carver <stan.carver@gatewaypeople.com>
2012-08-12 12:07:01 -0500

    added published and unpublished views. Moved page#index table to partial for reuse with published and unpublished views.

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,4 +1,4 @@
-class Api::PagesController < ApplicationController
+class Api::PagesController < ApplicationController  
   # GET /api/pages
   # GET /api/pages.json
   def index
@@ -7,6 +7,7 @@ class Api::PagesController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pages }
+      format.xml { render xml: @pages }
     end
   end
 
@@ -37,17 +38,24 @@ class Api::PagesController < ApplicationController
   # GET /api/pages/1/edit
   def edit
     @page = Page.find(params[:id])
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @page }
+      format.xml { render xml: @page }
+    end
   end
 
   # POST /api/pages
   # POST /api/pages.json
+  # POST /api/pages.xml
   def create
     @page = Page.new(params[:page])
 
     respond_to do |format|
       if @page.save
         format.html { redirect_to api_page_path(@page), notice: 'Page was successfully created.' }
-        format.json { render json: @page, status: :created, location: @page }
+        format.json { render json: @page, status: :created, location: api_page_path(@page) }
+        format.xml { render xml: @page, status: :created, location: api_page_path(@page) }
       else
         format.html { render action: "new" }
         format.json { render json: @page.errors, status: :unprocessable_entity }
@@ -60,7 +68,6 @@ class Api::PagesController < ApplicationController
   # PUT /api/pages/1.json
   def update
     @page = Page.find(params[:id])
-
     respond_to do |format|
       if @page.update_attributes(params[:page])
         format.html { redirect_to api_page_path(@page), notice: 'Page was successfully updated.' }
@@ -69,7 +76,7 @@ class Api::PagesController < ApplicationController
       else
         format.html { render action: "edit" }
         format.json { render json: @page.errors, status: :unprocessable_entity }
-        format.xml { render xml: @pages }
+        format.xml { render xml: @page.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -104,12 +111,28 @@ class Api::PagesController < ApplicationController
       format.xml { render xml: @pages }
     end
   end
+
+  def publish
+    @page = Page.find(params[:id])
+    respond_to do |format|
+      if @page.publish
+        format.html { redirect_to api_page_path(@page), notice: 'Page was successfully published.' }
+        format.json { head :no_content }
+        format.xml { head :no_content  }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+        format.xml { render xml: @page.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
   def total_words
     @page = Page.find(params[:id])
     respond_to do |format|
       format.html { render text: @page.total_word_count }
       format.json { render text: @page.total_word_count }
+      format.xml { render text: @page.total_word_count }
     end
   end
 end
