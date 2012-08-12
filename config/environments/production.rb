4677b38a3ca4186c597c5f184b42251f12f47614
Stan Carver <stan.carver@gatewaypeople.com>
2012-08-12 15:39:19 -0500

    added exception notiication.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ PagesApiApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.middleware.use ExceptionNotifier,
+    sender_address: "notifications@pagesapi.com",
+    exception_recipients: %w{stan@a1webconsulting.com}
+  
 end
