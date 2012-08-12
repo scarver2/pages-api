5f17fcc4b2a4db0b697503d5b672b9dba28f54a5
Stan Carver <stan.carver@gatewaypeople.com>
2012-08-12 12:03:17 -0500

    added Page seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..100).each do
+  Page.create!(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+end
