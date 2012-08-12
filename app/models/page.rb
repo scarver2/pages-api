f8a319b840556d2eb2388f7ead5b0bb2cb8ff269
Stan Carver <stan.carver@gatewaypeople.com>
2012-08-12 12:02:40 -0500

    added validations and publish/unpublish methods to Page model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,11 +1,20 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :published_on, :title
+  attr_accessible :content, :title
   
   validates_presence_of :content, :title
   validates_uniqueness_of :title
 
   scope :published, where('published_on IS NOT NULL')
   scope :unpublished, where('published_on IS NULL')
+  
+  def publish
+    # set publication date unless already set
+    update_attribute(:published_on, Time.now) unless published_on
+  end
+  
+  def unpublish
+    update_attribute(:published_on, nil)
+  end
   
   def total_word_count
     content.split.size + title.split.size
